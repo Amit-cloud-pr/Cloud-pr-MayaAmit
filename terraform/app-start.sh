a6688602f01d0444497cfb92df9212364cfb1e8a
Maya <mayash801@gmail.com>
2025-07-08 12:00:19 +0300

    working version

--- terraform/app-start.sh
+++ terraform/app-start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
+echo "=== STARTING APP ===" >> /var/log/my_app.log
 ### check path version if not changed!!
 echo "fixing path"
 export PATH=$PATH:/root/.nvm/versions/node/v22.17.0/bin
-cd /opt/Clouds-final-project/album-BE/ && . /root/envs.sh && nohup npm start &
-cd /opt/Clouds-final-project/album-FE/ && nohup npm start &
+cd /opt/Cloud-pr-MayaAmit/backend/ && . /root/envs.sh && npm start &
+cd /opt/Cloud-pr-MayaAmit/frontend/ && npm start &
